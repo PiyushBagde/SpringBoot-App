ed2edf7ce3418eb6d0ccae6ea07f23fb4be0a343
PiyushBagde <piyushbagde002@gmail.com>
2025-04-29 21:48:57 +0530

    removed unused imports

--- billing-service/src/main/java/com/supermarket/billingservice/exception/GlobalExceptionHandler.java
+++ billing-service/src/main/java/com/supermarket/billingservice/exception/GlobalExceptionHandler.java
@@ -2,14 +2,12 @@ package com.supermarket.billingservice.exception;
 
 import feign.FeignException;
 import jakarta.validation.ConstraintViolationException;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
